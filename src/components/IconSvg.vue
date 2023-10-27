801ddcfe5e4ee12c46e424cffe08c164d5a743a5
pakoval <pakoval@softserveinc.com>
2023-10-27 20:43:08 +0300

    add header && icons

---
+++ src/components/IconSvg.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg
+  class="icon"
+  :class="[`icon-${iconName}`, customClass]"
+  :width="width"
+  :height="height"
+  >
+    <use :xlink:href="`/sprite.svg#${iconName}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "IconSvg",
+  props: {
+    iconName: {
+      type: String,
+      default: "i",
+    },
+    customClass: {
+      type: String,
+      default: "",
+    },
+    width: {
+      type: [Number, String],
+      default: "16",
+    },
+    height: {
+      type: [Number, String],
+      default: "16",
+    }
+  }
+}
+</script>
